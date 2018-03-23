863191142925a143204b00184f427942cb2aaf8c
CarolineXWL89 <carolinexwliu89@hotmail.com>
2018-03-23 14:01:19 -0700

    Caroline #2 - URLReader

--- src/URLReader.java
+++ src/URLReader.java
@@ -1,9 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 /**
  * Created by princ on 23/03/2018.
  */
 public class URLReader {
 
-    public URLReader(){
-        
+    private String HTMLCode = "";
+
+    public URLReader(String URL){}
+
+    /**
+     * Takes the URL input and returns an HTML code.
+     * @param URL
+     * @return HTML code for webpage
+     */
+    public String readerReturn(String URL){
+        java.net.URL oracle = null;
+        BufferedReader in = null;
+        String inputLine;
+        try {
+            oracle = new URL(URL);
+            in = new BufferedReader(
+                    new InputStreamReader(oracle.openStream()));
+            while ((inputLine = in.readLine()) != null)
+                //System.out.println(inputLine);
+                HTMLCode += inputLine;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return HTMLCode;
     }
 }
