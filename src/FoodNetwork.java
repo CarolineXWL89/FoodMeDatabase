bb1487143ae0320425750b5f1eb6362dceb6bc05
michaelxiong <michaelxiong2001Gmail.com>
2018-03-25 20:48:52 -0700

    Michael 1: found all JSON start and end tags

--- src/FoodNetwork.java
+++ src/FoodNetwork.java
@@ -2,4 +2,8 @@
  * Created by princ on 25/03/2018.
  */
 public class FoodNetwork {
+
+    private static String startTag = "<script type=\"application/ld+json\">";
+    private static String endTag = "</script>";
+
 }
